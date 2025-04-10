e1d07b097ab3aef6c95ef9b4914484c724579cb8
Júlia <160253571+5456ju00@users.noreply.github.com>
2025-04-10 08:46:23 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,187 @@
+import java.util.Scanner;
+    public class soma {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o primeiro número inteiro:");
+            int numero1 = scanner.nextInt();
+
+            System.out.println("Digite o segundo número inteiro:");
+            int numero2 = scanner.nextInt();
+            
+            int soma = numero1 + numero2;
+            System.out.println("A soma dos dois números é: " + soma);
+
+            scanner.close();
+
+
+
+
+        }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Programa para saber se o aluno passou de ano");
+
+// var primeiroNumero = Number( prompt("Digite a primeira nota") );
+// var primeiroNumero = Number( prompt("Digite a segunda nota") );
+// var primeiroNumero = Number( prompt("Digite a terceiranota") );
+
+// var frequencia = Number(prompt("Digite a frequência do aluno"));
+
+// console.log( primeiraNota );
+// console.log( segundaNota );
+// console.log( terceiraNota );
+
+// var media = ( primeiranota + segundaNota + terceiraNota ) / 3;
+
+// console.log( media );
+// console.log( frequencia );
+
+// if ( media >= 6.0 && frequencia >= 75 ){
+ 
+//     alert("O aluno passou!");
+    
+// } else {
+
+//     alert("O aluno reprovou!");
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Calculadora");
+
+// var primeiroNumero = Number( prompt("Digite o primeiro número") );
+// var segundoNumero = Number( prompt("Digite o segundo número") );
+// var operacao = prompt("Digite a operção a ser realizada");
+
+// switch ( operacao ) {
+//     case "+":
+//         var soma = primeiroNumero + segundoNumero;
+//         console.log("A soma é:" + soma);
+//         alert("A soma é:" + soma);
+//     break;
+
+//     case "-":
+//         var subitracao = primeiroNumero - segundoNumero;
+//         console.log("A subitração é:" - subitracao);
+//         alert("A subitração é:" - subitracao);
+//     break;
+
+//     case "*":
+//         var multiplicacao = primeiroNumero * segundoNumero;
+//         console.log("A multiplicação é:" * multiplicacao);
+//         alert("A multiplicação é:" * multiplicacao);
+//     break;
+
+//     case "/":
+//         var divisao = primeiroNumero / segundoNumero;
+//         console.log("A divisão é:" / divisao);
+//         alert("A divisão é:" / divisao);
+//     break;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
